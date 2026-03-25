b903200620406547179dab60764bc22197b6b8ff
shamal2005 <mohdshamal13@gmail.com>
2026-03-25 09:14:17 +0530

    UC1: Add welcome message module

--- src/booktrain.java
+++ src/booktrain.java
@@ -1,3 +1,11 @@
 public class booktrain {
 
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Book My Stay App");
+        System.out.println("Hotel Booking Management System");
+        System.out.println("Version: 1.0");
+        System.out.println("=================================");
+    }
 }
