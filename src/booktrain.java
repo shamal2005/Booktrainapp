25d95af0444a360d7e736f2cf9780d056365615a
shamal2005 <mohdshamal13@gmail.com>
2026-03-25 09:54:13 +0530

    UC2:case2

--- src/booktrain.java
+++ src/booktrain.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+
 public class booktrain {
 
     public static void main(String[] args) {
 
-        System.out.println("=================================");
-        System.out.println("Welcome to Book My Stay App");
-        System.out.println("Hotel Booking Management System");
-        System.out.println("Version: 1.0");
-        System.out.println("=================================");
+        ArrayList<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println("Passenger Bogies after addition:");
+        System.out.println(passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println("\nPassenger Bogies after removal:");
+        System.out.println(passengerBogies);
+
+        boolean exists = passengerBogies.contains("Sleeper");
+        System.out.println("\nDoes Sleeper bogie exist? " + exists);
+
+        System.out.println("\nFinal Passenger Bogies List:");
+        System.out.println(passengerBogies);
     }
 }
